f8021c4e251bdde7a1ce4cdd352edb59e52fa28d
Mategyorffy <95756877+Mategyorffy@users.noreply.github.com>
2022-01-24 10:07:59 +0100

    Enemy Movement

--- Assets/Code/EnemyMovement.cs
+++ Assets/Code/EnemyMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 namespace GameMechanics {
 	public class EnemyMovement : MovementBase {
 		protected override Vector2 GetDirection() {
-			//..
 
+			Vector2 direction;
+			direction.x = Input.GetAxisRaw("Horizontal");
+			direction.y = Input.GetAxisRaw("Vertical");
 
-			//
-
-			return Vector2.right;
+			return direction;
+		
+			
 		}
 	}
 }
