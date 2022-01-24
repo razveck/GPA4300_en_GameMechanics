e80d61350f463cf9a2e1764873e7e44719014a11
razveck <razveck@gmail.com>
2022-01-24 13:02:27 +0100

    Added aiming

---
+++ Assets/Code/PlayerAiming.cs
@@ -0,0 +1,32 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GameMechanics {
+	public class PlayerAiming : MonoBehaviour {
+
+		// Use this for initialization
+		private void Start() {
+
+		}
+
+		// Update is called once per frame
+		private void Update() {
+			Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+			Vector3 direction = Input.mousePosition - playerScreenPosition;
+
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+			Vector3 rotation = transform.rotation.eulerAngles;
+			rotation.z = angle;
+
+			transform.rotation = Quaternion.Euler(rotation);
+		}
+	}
+}
